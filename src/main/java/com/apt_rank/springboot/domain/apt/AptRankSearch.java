546a3e3ba68d39a40da7dc187bce0b7e8cd9139e
hyunjin park <hyyyyjin@naver.com>
2021-01-03 11:46:15 +0900

    add db info (apt_search)

--- src/main/java/com/apt_rank/springboot/domain/apt/AptRankSearch.java
+++ src/main/java/com/apt_rank/springboot/domain/apt/AptRankSearch.java
@@ -3,106 +3,62 @@ package com.apt_rank.springboot.domain.apt;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Getter
 @NoArgsConstructor
 @Entity
-@Table
+@Table(name="apt_trans_rank")
+@IdClass(AptTransRank_Id.class)
 public class AptRankSearch {
 
     @Id
-    @Column(length=5, nullable = false, name = "s_apt_name")
-    private String s_apt_name;
+    @Column(nullable = false, name = "rank")
+    private int rank;
 
-    @Column(length=5, nullable = false, name = "my_k_rank")
-    private String my_k_rank;
+    @Id
+    @Column(nullable = false, name ="op_dt")
+    private Date op_dt;
 
-    @Column(length=5, nullable = false, name = "k_total")
-    private String k_total;
+    @Column(nullable = true, length = 30, name ="serial_num")
+    private String serial_num;
 
-    @Column(length=5, nullable = false, name = "my_gu_rank")
-    private String my_gu_rank;
+    @Column(nullable = false, length = 45, name ="apt_name")
+    private String apt_name;
 
-    @Column(length=5, nullable = false, name = "gu_total")
-    private String gu_total;
+    @Column(nullable = true, length = 45, name ="apt_floor")
+    private String apt_floor;
 
-    @Column(length=5, nullable = false, name = "s_trans_yymm")
-    private String s_trans_yymm;
+    @Column(nullable = true, length = 45, name ="exclusive_area")
+    private String exclusive_area;
 
-    @Column(length=5, nullable = false, name = "s_address1")
-    private String s_address1;
+    @Column(nullable = false, length = 45, name ="trans_yymm")
+    private String trans_yymm;
 
-    @Column(length=5, nullable = false, name = "s_address2")
-    private String s_address2;
+    @Column(nullable = false, length = 45, name ="trans_yymmdd")
+    private String trans_yymmdd;
 
-    @Column(length=5, nullable = false, name = "s_address3")
-    private String s_address3;
+    @Column(nullable = false, length = 45, name ="trans_price")
+    private String trans_price;
 
-    @Column(length=5, nullable = false, name = "s_price")
-    private String s_price;
+    @Column(nullable = false, length = 45, name ="addr_cd")
+    private String addr_cd;
 
-    @Column(length=5, nullable = false, name = "gu_ex_price")
-    private String gu_ex_price;
+    @Column(nullable = false, length = 45, name ="addr_pr_cd")
+    private String addr_pr_cd;
 
-    @Column(length=5, nullable = false, name = "gu_ex_apt_name")
-    private String gu_ex_apt_name;
+    @Column(nullable = false, length = 45, name ="addr_ct_cd")
+    private String addr_ct_cd;
 
-    @Column(length=5, nullable = false, name = "gu_ex_trans_yymm")
-    private String gu_ex_trans_yymm;
+    @Column(nullable = false, length = 45, name ="addr_dong_cd")
+    private String addr_dong_cd;
 
-    @Column(length=5, nullable = false, name = "gu_ex_address3")
-    private String gu_ex_address3;
+    @Column(nullable = false, length = 45, name ="unit_price")
+    private String unit_price;
 
-    @Column(length=5, nullable = false, name = "gu_cp_price")
-    private String gu_cp_price;
+    @Column(nullable = false, name ="audit_dtm")
+    private Date audit_dtm;
 
-    @Column(length=5, nullable = false, name = "gu_cp_apt_name")
-    private String gu_cp_apt_name;
-
-    @Column(length=5, nullable = false, name = "gu_cp_trans_yymm")
-    private String gu_cp_trans_yymm;
-
-    @Column(length=5, nullable = false, name = "gu_cp_address3")
-    private String gu_cp_address3;
-
-    @Column(length=5, nullable = false, name = "k_ex_price")
-    private String k_ex_price;
-
-    @Column(length=5, nullable = false, name = "k_ex_apt_name")
-    private String k_ex_apt_name;
-
-    @Column(length=5, nullable = false, name = "k_ex_trans_yymm")
-    private String k_ex_trans_yymm;
-
-    @Column(length=5, nullable = false, name = "k_ex_address1")
-    private String k_ex_address1;
-
-    @Column(length=5, nullable = false, name = "k_ex_address2")
-    private String k_ex_address2;
-
-    @Column(length=5, nullable = false, name = "k_ex_address3")
-    private String k_ex_address3;
-
-    @Column(length=5, nullable = false, name = "k_cp_price")
-    private String k_cp_price;
-
-    @Column(length=5, nullable = false, name = "k_cp_apt_name")
-    private String k_cp_apt_name;
-
-    @Column(length=5, nullable = false, name = "k_cp_trans_yymm")
-    private String k_cp_trans_yymm;
-
-    @Column(length=5, nullable = false, name = "k_cp_address1")
-    private String k_cp_address1;
-
-    @Column(length=5, nullable = false, name = "k_cp_address2")
-    private String k_cp_address2;
-
-    @Column(length=5, nullable = false, name = "k_cp_address3")
-    private String k_cp_address3;
 
 }
--- src/main/java/com/apt_rank/springboot/domain/apt/AptTransRank_Id.java
+++ src/main/java/com/apt_rank/springboot/domain/apt/AptTransRank_Id.java
@@ -1,2 +1,17 @@
-package com.apt_rank.springboot.domain.apt;public class AptTransRank_Id {
+package com.apt_rank.springboot.domain.apt;
+
+
+import lombok.Data;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Embeddable
+public class AptTransRank_Id implements Serializable {
+
+    private int rank;
+    private String op_dt;
+
 }
